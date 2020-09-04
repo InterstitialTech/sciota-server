8d25996f600c9daee7016c05f91357bfb5fcfc65
Ben Burdette <bburdette@gmail.com>
2020-09-04 16:15:49 -0600

    default.nix cleanup

--- nixpkg/default.nix
+++ nixpkg/default.nix
@@ -1,55 +1,28 @@
 { stdenv
-, fetchgit
+, fetchFromGitHub
 , rustPlatform
 , openssl
 , pkgconfig
 , sqlite
 , callPackage }:
 
-# , lib
-# , packr
-
 rustPlatform.buildRustPackage rec {
   pname = "sciota-server";
   version = "1.0";
 
-  src = fetchgit {
-    url = "https://github.com/InterstitialTech/sciota-server.git";
-    rev = "3e78330f3b19428443358b3829c769f76dbe1d69";
-    sha256 = "14d2pvf81kq54h8wvmxwc5dljc8yhrhx2v2q4vzc9rpawmsf7f4r";
+  src = fetchFromGitHub {
+    owner = "InterstitialTech";
+    repo = "sciota-server";
+    rev = "dcc0821b145a62592afe1a8c9db88d33c4d987be";
+    sha256 = "1fg4vv1wybwk0rz9dnxyg49wmh5kpma8il6rjxmh5dv8c0wpah30";
     fetchSubmodules = true;
   };
-  # src = fetchFromGitHub {
-  #   owner = "InterstitialTech";
-  #   repo = "sciota-server";
-  #   rev = "18c4181f0228afcd152f1ae3754e8825ac8b3567";
-  #   sha256 = "14lncsrnvfcarlrh9dwkv33nwcf0xxsc8ysalvxrrv7d6sd90kgb";
-  #   fetchSubmodules = true;
-  # };
-  # 3be7e31c95d0bc945a73ec27af608cea216da69c
-  # 3be7e31c95d0bc945a73ec27af608cea216da69c
-  # ui = callPackage ./ui.nix { };
-  the_elm = callPackage "${src}/elm" {  };
 
-  # preBuild = ''
-  #   cp -r ${ui}/libexec/gotify-ui/deps/gotify-ui/build ui/build && packr
-  # '';
-
-  postInstall = ''
-    echo "postInstall"
-    ls -l $out
-    mkdir $out/static
-    cp -r ${src}/server/static $out/static
-    cp -r ${the_elm}/Main.js $out/static/main.js
-  '';
-
-  # cargo-culting this from the gotify package.
   subPackages = [ "." ];
 
 
-  sourceRoot = "${src}/server";
-  cargoSha256 = "1jdbjx3xa7f4yhq4l7xsgy6jpdr2lkgqrzarqb5vj2s3jg13kyl4";
-  # dontMakeSourcesWritable=1;
+  sourceRoot = "source/server";
+  cargoSha256 = "1dah7ybr7qqfj3gdri26zn5b2cj6rvdcsdsxgn3sw6aa24cnykvs";
 
   buildInputs = [openssl sqlite];
 
@@ -58,7 +31,7 @@ rustPlatform.buildRustPackage rec {
   meta = with stdenv.lib; {
     description = "A measurement repository for IOT devices.";
     homepage = https://github.com/InterstitialTech/sciota-server;
-    license = with licenses; [ bsd3 ];
+    license = with licenses; [ gpl3 ];
     maintainers = [ bburdette chronopoulos ];
     platforms = platforms.all;
   };
