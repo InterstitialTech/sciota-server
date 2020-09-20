84e32ecb0b09795db363d719701953ee3fe86efd
Ben Burdette <bburdette@gmail.com>
2020-09-19 20:07:47 -0600

    mr eval

--- server/src/expression.rs
+++ server/src/expression.rs
@@ -1,7 +1,9 @@
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Moment {
   pub time: i64,
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub enum At {
   Before(Moment),
   After(Moment),
@@ -11,6 +13,7 @@ pub enum At {
   Last,
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub enum ArithBinaryOp {
   Add,
   Subtract,
@@ -18,16 +21,50 @@ pub enum ArithBinaryOp {
   Divide,
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub enum BoolOp {
   GT,
   LT,
   EQ,
 }
 
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub enum Expression {
   Const(f64),
   Measurement { sensor: i64, at: At },
   MeasurementRange { sensor: i64, from: At, to: At },
   Abo(ArithBinaryOp, Box<Expression>, Box<Expression>),
   Bo(BoolOp, Box<Expression>, Box<Expression>),
+}
+
+pub enum MeasureRequest {
+  Single { sensor: i64, at: At },
+  Range { sensor: i64, from: At, to: At },
+}
+
+pub fn eval_for_measure_requests(exp: &Expression) -> Vec<MeasureRequest> {
+  match exp {
+    Expression::Const(_) => vec![],
+    Expression::Measurement { sensor, at } => vec![MeasureRequest::Single {
+      sensor: sensor.clone(),
+      at: at.clone(),
+    }],
+    Expression::MeasurementRange { sensor, from, to } => vec![MeasureRequest::Range {
+      sensor: sensor.clone(),
+      from: from.clone(),
+      to: to.clone(),
+    }],
+    Expression::Abo(op, exp1, exp2) => {
+      let mut v = eval_for_measure_requests(exp2);
+      let mut v2 = eval_for_measure_requests(exp1);
+      v2.append(&mut v);
+      v2
+    }
+    Expression::Bo(op, exp1, exp2) => {
+      let mut v = eval_for_measure_requests(exp2);
+      let mut v2 = eval_for_measure_requests(exp1);
+      v2.append(&mut v);
+      v2
+    }
+  }
 }
